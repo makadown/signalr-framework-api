0115c436673f3dd192dd1c81d5e0795d8823bda7
makadown <maseflores79@gmail.com>
2020-06-11 09:38:16 -0500

    agregando readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# Proyecto Web API signalR con net Framework
+
+Este proyecto es parte de un research para utilizar signalR con net Framework, dado que ahora toda documentación actualmente está para dotnet Core.
+En un futuro, haré una investigación aparte de SignalR con dotnet core.
+
+# Referencias
+
+https://docs.microsoft.com/en-us/aspnet/core/signalr/version-differences?view=aspnetcore-3.1
---
+++ signalR-framework-test/Hubs/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace signalR_framework_test.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
